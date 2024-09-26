61efbd4b0bccfc4790f492c75167e9b7477f2972
Changyo Han <k9s1j7@gmail.com>
2024-09-26 11:01:23 +0900

    fix integrating launch file

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
+from setuptools import find_packages, setup
 import os
 from glob import glob
-from setuptools import setup
 
-package_name = 'bsactuator'
+package_name = 'bsactuator_ros'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py')))
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
