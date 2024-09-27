1adbb76d65693abb81166285770eaa5d5f7e78e8
Changyo Han <k9s1j7@gmail.com>
2024-09-27 22:50:12 +0900

    remove actual device for test

--- bsactuator_ros/bsactuator_ros.py
+++ bsactuator_ros/bsactuator_ros.py
@@ -15,7 +15,7 @@ class BsActuatorNode(Node):
         self.get_logger().set_level(logging.DEBUG)
 
         # Initialize the bsactuator
-        self.ba = bsactuator.BsActuator("/dev/bambooshoot_actuator", 115200, self.model)
+        # self.ba = bsactuator.BsActuator("/dev/bambooshoot_actuator", 115200, self.model)
 
         # Setting subscribers
         self.get_logger().info("Setting subscribers...")
@@ -47,26 +47,32 @@ class BsActuatorNode(Node):
 
         if self.model == "50mm02":
             self.moving = True
-            self.ba.set_length(msg.data)
+            # self.ba.set_length(msg.data)
+            self.get_logger().info(f"setting actuator length: {msg.data} mm")
 
     def hold_callback(self, msg):
         if msg.data:
-            self.ba.hold()
+            # self.ba.hold()
+            self.get_logger().info(f"actuator hold")
 
     def release_callback(self, msg):
         if msg.data:
-            self.ba.release()
+            # self.ba.release()
+            self.get_logger().info(f"actuator release")
 
     def reset_callback(self, msg):
         if msg.data:
-            self.ba.reset()
+            # self.ba.reset()
+            self.get_logger().info(f"actuator reset")
 
     def stop_callback(self, msg):
         if msg.data:
-            self.ba.stop()
+            # self.ba.stop()
+            self.get_logger().info(f"actuator stop")
 
     def publish_get_length(self):
-        self.current_length = int(self.ba.get_length())
+        # self.current_length = int(self.ba.get_length())
+        self.current_length = int(333)
         self.publisher_get_length.publish(Int16(data=self.current_length))
         if self.moving and abs(self.current_length - self.dist_length) < 10:
             self.moving = False
