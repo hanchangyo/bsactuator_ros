54dff6a89cf49c8db510c6bdab3b0c9986abfeb4
Changyo Han <k9s1j7@gmail.com>
2024-09-30 16:27:42 +0900

    update status implementation

--- bsactuator_ros/bsactuator_ros.py
+++ bsactuator_ros/bsactuator_ros.py
@@ -1,18 +1,14 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16, Bool
+from std_msgs.msg import Int16, Bool, String
 from bsactuator import bsactuator
 import time
-import logging
 
 class BsActuatorNode(Node):
 
     def __init__(self):
         super().__init__('bsactuator_ros')
         self.model = "50mm02"
-
-        # Set logging level
-        self.get_logger().set_level(logging.DEBUG)
 
         # Initialize the bsactuator
         self.ba = bsactuator.BsActuator("/dev/bambooshoot_actuator", 115200, self.model)
@@ -28,6 +24,7 @@ class BsActuatorNode(Node):
         # Setting publishers
         self.get_logger().info("Setting publishers...")
         self.publisher_get_length = self.create_publisher(Int16, 'bsactuator/length', 10)
+        self.publisher_goal_status = self.create_publisher(String, 'bsactuator/status', 10)  # Added publisher for status
 
         self.moving = False
         self.current_length = 0
@@ -48,6 +45,7 @@ class BsActuatorNode(Node):
         if self.model == "50mm02":
             self.moving = True
             self.ba.set_length(msg.data)
+            self.get_logger().info("Actuator moving...")
 
     def hold_callback(self, msg):
         if msg.data:
@@ -68,10 +66,19 @@ class BsActuatorNode(Node):
     def publish_get_length(self):
         self.current_length = int(self.ba.get_length())
         self.publisher_get_length.publish(Int16(data=self.current_length))
+
         if self.moving and abs(self.current_length - self.dist_length) < 10:
             self.moving = False
             self.get_logger().info("Goal reached.")
 
+            # Publish the status to indicate success
+            status_msg = String()
+            status_msg.data = "Goal reached successfully."
+            self.publisher_goal_status.publish(status_msg)
+
+        elif self.moving and abs(self.current_length - self.dist_length) >= 10:
+            # If it's still moving but hasn't reached the target
+            self.get_logger().info("Actuator still moving.")
 
 def main(args=None):
     rclpy.init(args=args)
